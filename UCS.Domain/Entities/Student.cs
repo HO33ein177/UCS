80afdc511f5631d3b81728a92cf2eb8c49331bf7
Hosein <hoseinbm138084@gmail.com>
2024-08-24 11:46:19 +0330

    Initial Commit

---
+++ UCS.Domain/Entities/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace UCS.Domain.Entities;
+
+public class Student
+{
+    public string? FullName { get; set; }
+    
+    public long StudentId { get; set; }
+
+    private string Degree { get; set; }
+
+    private string Major { get; set; }
+}
---
+++ UniCourseSelect/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UniCourseSelect.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
